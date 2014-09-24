24cf5c3358e743254b53d3c06c8de6bd665ca150
zaki-yama <shingoyamazaki00@gmail.com>
2014-09-25 00:09:01 +0900

    fixed URL conf

--- django_apps/gmail_actions_in_the_inbox/urls.py
+++ django_apps/gmail_actions_in_the_inbox/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns
 from .views import ReviewHandlerView, EmailSenderView
 
 urlpatterns = patterns('',
+    (r'^email', EmailSenderView.as_view()),
     (r'^', ReviewHandlerView.as_view()),
-    (r'^email/', EmailSenderView.as_view()),
 )
