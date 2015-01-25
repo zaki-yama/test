680741f6fbb2e302b02579b93fa5ea2f97bfa61d
zaki-yama <shingoyamazaki00@gmail.com>
2015-01-25 17:44:57 +0900

    tweak on urls.py

--- urls.py
+++ urls.py
@@ -5,14 +5,13 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-	url(r'^class_based_view_csrf/', include('class_based_view_csrf.urls')),
-	url(r'^', include('guestbook.urls')),
-    # url(r'^gae_django_app/', include('gae_django_app.foo.urls')),
+		url(r'^class_based_view_csrf/', include('class_based_view_csrf.urls')),
+		url(r'^', include('guestbook.urls')),
+		# url(r'^gae_django_app/', include('gae_django_app.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+		# Uncomment the admin/doc line below to enable admin documentation:
+		# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+		# Uncomment the next line to enable the admin:
+		# url(r'^admin/', include(admin.site.urls)),
 )
