0e2112c5eeda49f64d1fa6c1a8f31e75a718de5d
zaki-yama <shingoyamazaki00@gmail.com>
2015-01-29 00:58:44 +0900

    タスク一覧が表示できるようになった

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+import httplib2
 import logging
 import os
+import settings
 
 from google.appengine.api import users
 
 from apiclient.discovery import build
+from oauth2client import xsrfutil
 from oauth2client.appengine import StorageByKeyName
 from oauth2client.client import flow_from_clientsecrets
 
+from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 
 from .models import CredentialsModel
@@ -27,22 +32,38 @@ FLOW = flow_from_clientsecrets(
 
 
 class MainView(View):
+	template_name = 'task_manager/index.html'
 
 	def get(self, request):
-		if users.get_current_user():
+		user = users.get_current_user()
+		storage = StorageByKeyName(
+				CredentialsModel, user.email(), 'credentials')
+		credentials = storage.get()
+		if credentials is None or credentials.invalid == True:
+			FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
+					user)
 			authorize_url = FLOW.step1_get_authorize_url()
-			logger.info(authorize_url)
+			logging.info(authorize_url)
 			return HttpResponseRedirect(authorize_url)
 		else:
-			login_url = users.create_login_url(request.get_full_path())
-			return  HttpResponseRedirect(login_url)
+			http = httplib2.Http()
+			http = credentials.authorize(http)
+			service = build('tasks', 'v1', http=http)
+			tasks = service.tasks().list(tasklist='@default').execute(http=http)
+			template_values={
+					'app_name':request.resolver_match.app_name,
+					'tasks':tasks['items'],
+					}
+			template_values.update(csrf(request))
+			return render(request, self.template_name, template_values)
 
 
 class AuthView(View):
 
 	def get(self, request):
+		user = users.get_current_user()
 		credentials = FLOW.step2_exchange(request.REQUEST)
 		storage = StorageByKeyName(
-				CredentialsModel, 'admin@example.com', 'credentials')
+				CredentialsModel, user.email(), 'credentials')
 		storage.put(credentials)
-		return HttpResponseRedirect('/')
+		return HttpResponseRedirect('/task_manager')
